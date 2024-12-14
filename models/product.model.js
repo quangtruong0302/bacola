74f8e686b8308a3fd5e178febd6d97b3e7dbcb03
quangtruong0302 <buiquangtruong170302@gmail.com>
2024-12-14 10:00:07 +0700

    .

---
+++ models/product.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+const productSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    category: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      account_id: String,
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    slug: { type: String, slug: "title", unique: true },
+    updatedBy: [
+      {
+        account_id: String,
+        updatedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    deletedBy: {
+      account_id: String,
+      deletedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema, "products");
+module.exports = Product;
